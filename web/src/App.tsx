bfcedb9b09d881d3dca829a6a8d3d16244950e8f
RFHold <robert@robhold.com>
2022-03-18 11:13:24 -0400

    testing grpc-web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
+import { MessageServiceClient } from 'lib/api/messages/v1/Message_serviceServiceClientPb';
+import { CreateMessageRequest } from 'lib/api/messages/v1/message_service_pb';
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const client = new MessageServiceClient('http://localhost:8080')
+
+  const newMessage = async (content: string) => {
+    const request = new CreateMessageRequest();
+    request.setContent(content);
+    const response = await client.createMessage(request, null);
+    console.log(response);
+  };
 
   return (
     <div className="App">
